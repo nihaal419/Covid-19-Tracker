beb90c5a5c2a2ef79533755e57a15d1dc374fe0a
Nihaal Manesia <nihaal@manesia.org>
2020-04-01 17:22:09 -0500

    Initial Commit

---
+++ Covid-19 Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19 Tracker
+//
+//  Created by Nihaal Manesia on 4/1/20.
+//  Copyright © 2020 Rebytalized. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
